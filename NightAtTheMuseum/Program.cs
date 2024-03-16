78c75ceb67b62a5edee28247383f9283af9ba8d4
unknown <islammohamedahmed49@gmail.com>
2024-03-16 17:30:45 +0200

    night at the museum

---
+++ NightAtTheMuseum/Program.cs
@@ -0,0 +1,12 @@
+﻿char start = (char)97;
+int sum = 0;
+string str = Console.ReadLine();
+for (int i = 0; i < str.Length; i++)
+{
+
+    int helper = Math.Abs(str[i] - start);
+    int mn = Math.Min(helper, 26 - helper);
+    sum += mn;
+    start = (char)str[i];
+}
+Console.WriteLine(sum);
